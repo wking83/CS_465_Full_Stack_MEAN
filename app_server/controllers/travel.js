1c8d921b484db9eb450789aa4f128bd1609f048d
Michael Clerico <mclerico1983@gmail.com>
2025-05-19 19:58:06 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,10 @@
-/* GET travel view  */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+/*get travel view*/
 const travel = (req, res) => {
     console.log('Inside app_server, controllers, travel.js travel function.');
-    res.render('travel', {title: "Travlr Getaways"});
+    res.render('travel', {title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
